d0a75902a08fa9a3b5ea7de2230b48cb4537807c
Angela Gola <angelagola.ko@gmail.com>
2022-09-10 22:41:13 -0400

    Working on routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SignUp from './pages/Signup';
 import Navigation from './components/Navigation';
+import SignUp from './pages/Signup';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <div className="flex-column">
         <Navigation/>
         <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} /> */}
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </div>
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container"
 import NavBar from "react-bootstrap/Navbar"
 import { useState, useEffect } from "react"
 import { Link, Outlet } from "react-router-dom"
+import Auth from "../utils/auth";
 
 const Navigation = (props) => {
 
@@ -10,9 +11,30 @@ const Navigation = (props) => {
 return (
     <NavBar>
         <Container>
-            <NavBar.Brand as={Link} to="/login">
+            <NavBar.Brand as={Link} to="/">
                 Artem Lab
             </NavBar.Brand>
+
+            <Nav.Link href="#home" as={Link} to="/">
+                Home
+            </Nav.Link>
+
+            {Auth.loggedIn() ? (
+                <>
+                <Nav.Link href="#draw" as={Link} to="/draw">
+                    Draw
+                </Nav.Link>
+                </>
+            ) : (
+                <>
+                <Nav.Link href="#login" as={Link} to="/login">
+                    Login
+                </Nav.Link>
+                <Nav.Link href="#signup" as={Link} to="/signup">
+                    Sign Up
+                </Nav.Link>
+                </>
+            )}
         </Container>
     </NavBar>
 )
