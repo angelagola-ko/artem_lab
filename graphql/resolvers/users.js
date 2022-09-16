360cec786a39eac7f86c8d17ad4bbab1dde65f15
Angela Gola <angelagola.ko@gmail.com>
2022-09-16 12:11:35 -0400

    Added function so username cannot be added twice

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
@@ -10,10 +11,18 @@ module.exports = {
     Mutation: {
         async register(_, 
             { registerInput:{ username, email, password, confirmPassword } },
-             context, info) {
+             ) {
             //TODOO validate user data
             // TODO Make sure user doesnt already exist
-            //TODO hash password and create an auth token
+            const user = await User.findOne({ username });
+            if(user){
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'This username is taken'
+                    }
+                })
+            }
+            // hash password and create an auth token
             password = await bcrypt.hash(password, 10);
 
             const newUser = new User ({
