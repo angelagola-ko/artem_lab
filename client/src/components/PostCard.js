c9f34b4c350d579a7ffa3d748b67d9d166c415df
Angela Gola <angelagola.ko@gmail.com>
2022-09-21 19:34:37 -0400

    Posts are loading on homepage

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+
+function PostCard({ post: { body, createdAt, id, username, likeCount, commentCount, likes}}){
+
+    return (
+        <Card fluid>
+        <Card.Content>
+          <Image
+            floated='right'
+            size='mini'
+            src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+          />
+          <Card.Header>{username}</Card.Header>
+          <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
+          <Card.Description>
+            {body}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <p>buttons here</p>
+        </Card.Content>
+      </Card>
+    )
+}
+
+export default PostCard;
