627783d9aed683affee573daa9f69fa1e43acde8
Angela Gola <angelagola.ko@gmail.com>
2022-09-22 11:47:44 -0400

    Register working and routing to homepage

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -4,7 +4,10 @@ import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
 
-function Register() {
+function Register(props) {
+
+    const [errors, setErrors] = useState({});
+
     const [values, setValues] = useState({
         username: '',
         password: '',
@@ -15,55 +18,79 @@ function Register() {
     const onChange = (event) => {
         setValues({ ...values, [event.target.name]: event.target.value});
     }
+    
+        const [addUser, { loading }] = useMutation(REGISTER_USER, {
+            update(_, result){
+                console.log(result);
+                props.history.push('/');
+            },
+            onError(err){
+                console.log(err.graphQLErrors[0].extensions.exception.errors)
+                setErrors(err.graphQLErrors[0].extensions.exception.errors);
+
+            },
+            variables: values 
+        })
 
     const onSubmit = (event) => {
         event.preventDefault();
+        addUser()
     }
-
-    const [addUser, { loading }] = useMutation(REGISTER_USER, {
-        update(proxy, result){
-            console.log(result)
-        },
-        variables: {
-            username: values.username
-        }
-    })
     return (
-        <div>
-            <Form onSubmit={onSubmit} noValidate>
+        <div className='form-container'>
+            <Form onSubmit={onSubmit} noValidate className={loading ? "loading" : ""}>
                 <h1>Register</h1>
                 <Form.Input
                 label="username"
                 placeholder="Username..."
                 name="username"
+                type="text"
                 value={values.username}
+                error={errors.username ? true : false}
                 onChange={onChange}
                 />
                  <Form.Input
                 label="Email"
                 placeholder="Email..."
                 name="email"
+                type="email"
                 value={values.email}
+                error={errors.email ? true : false}
                 onChange={onChange}
                 />
                  <Form.Input
                 label="Password"
                 placeholder="Password..."
                 name="password"
+                type="password"
                 value={values.password}
+                error={errors.password ? true : false}
                 onChange={onChange}
                 />
                  <Form.Input
                 label="Confirm Password"
                 placeholder="Confirm Password..."
                 name="confirmPassword"
+                type="password"
                 value={values.confirmPassword}
+                error={errors.confirmPassword ? true : false}
                 onChange={onChange}
                 />
                 <Button type="submit" primary>
                     Register
                 </Button>
             </Form>
+
+        {Object.keys(errors).length > 0 && (
+                <div className="ui error message">
+                <ui className="list">
+                    {Object.values(errors).map(value => (
+                        <li key={value}>{value}</li>
+                    ))}
+                </ui>
+            </div>
+
+        )} 
 
         </div>
     )
