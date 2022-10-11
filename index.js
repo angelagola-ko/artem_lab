57aee3d65f1a0a644cf9a7b922eb80bde94a180d
Angela Gola <angelagola.ko@gmail.com>
2022-10-10 21:10:48 -0400

    Merge branches

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const resolvers = require('./graphql/resolvers');
 const typeDefs = require('./graphql/typeDefs');
 const { MONGODB } = require('./config');
 
-
+const PORT = process.env.port || 5000;
 
 
 const server = new ApolloServer({
@@ -17,8 +17,11 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, { useNewUrlParser: true})
 .then(() => {
     console.log('MongoDB connected')
-    return server.listen({ port: 5000 })
+    return server.listen({ port: PORT })
 })
 .then((res) => {
     console.log(`Server running at ${res.url}`)
-});
+})
+.catch(err => {
+    console.error(err)
+})
